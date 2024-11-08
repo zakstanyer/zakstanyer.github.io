9dd4465f5db8ca712f270d3a8d431ee730bf3963
Zak Stanyer <zs262342@falmouth.ac.uk>
2024-11-08 20:40:35 +0000

    frontend files
    files

---
+++ Clients/GenresClient.cs
@@ -0,0 +1,9 @@
+﻿using GameBlog.Frontend.Models;
+
+namespace GameBlog.Frontend.Clients;
+
+public class GenresClient(HttpClient httpClient)
+{
+    public async Task<Genre[]> GetGenresAsync()
+        => await httpClient.GetFromJsonAsync<Genre[]>("genres") ?? [];
+}
